4f8f752841e1b415a726777ada2946073aad8e5b
SilvesterPPn <uhanaelijah3@gmail.com>
2025-08-09 17:50:44 +0100

    Latest Media Screen Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,23 @@
 /* --------------SHOW MENU--------------- */
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
 
 /* --------------Menu Show--------------- */
+/* --------------  validate if constat exists--------------- */
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  })
+}
 
 /* --------------HIDE SHOW--------------- */
-
+/* --------------  validate if constat exists--------------- */
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  })
+}
 /* --------------IMAGE GALLERY--------------- */
 function imgGallery() {
   const mainImg = document.querySelector('.details__img'),
@@ -28,13 +42,21 @@ var swiperCategories = new Swiper(".categories__container", {
   },
 
   breakpoints: {
-    640: {
+    350: {
       slidesPerView: 2,
-      spaceBetween: 20,
+      spaceBetween: 24,
     },
     768: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+    992: {
       slidesPerView: 4,
-      spaceBetween: 40,
+      spaceBetween: 24,
+    },
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 24,
     },
     1400: {
       slidesPerView: 6,
@@ -53,13 +75,13 @@ var swiperProducts = new Swiper(".new__container", {
   },
 
   breakpoints: {
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
     768: {
-      slidesPerView: 4,
-      spaceBetween: 40,
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 24,
     },
     1400: {
       slidesPerView: 4,
@@ -76,8 +98,7 @@ tabs.forEach((tab) => {
     tab.addEventListener('click', () => {
       const target = document.querySelector(tab.dataset.target);
       tabContents.forEach((tabContent) => {
-        const every = tabContent.classList.remove('active-tab');
-        console.log(every)
+         tabContent.classList.remove('active-tab');
       });
       target.classList.add('active-tab');
 
